28fe78bd0f0bcdc4576013fe26f5b598650f3cdc
4nh_H4v3rtz <121730782+AnhHavertz@users.noreply.github.com>
2023-06-22 16:56:49 +0700

    Update Schema2.sql

--- 3/Schema2.sql
+++ 3/Schema2.sql
@@ -1,81 +1,54 @@
-import mysql.connector
-import xml.etree.ElementTree as ET
-import time
-from tabulate import tabulate
+drop database if exists TRUONGHOC2;
 
-def query_database():
-    # Người dùng nhập thông tin
-    input_str = input("Nhập tên cơ sở dữ liệu, tên lĩnh vực, năm học, xếp hạng học tập (cách nhau bằng dấu phẩy): ")
-
-    # Tách chuỗi nhập vào thành các phần tử riêng biệt
-    inputs = input_str.split(",")
-
-    # Gán giá trị cho các biến tương ứng
-    database_name = inputs[0].strip()
-    ten_truong = inputs[1].strip()
-    nam_hoc = inputs[2].strip()
-    xep_loai = inputs[3].strip()
-
-    # Thiết lập kết nối với cơ sở dữ liệu MySQL
-    conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="211031",
-        database="truonghoc1"
-    )
-
-    # Tạo con trỏ để thực hiện các truy vấn
-    cursor = conn.cursor()
-
-    # Đo thời gian thực hiện truy vấn
-    start_time = time.time()
-
-    # Truy vấn dữ liệu từ cơ sở dữ liệu
-    query = f"""
-            SELECT hs.ho, hs.ten, hs.ntns, hoc.diemtb, hoc.xeploai, hoc.kqua
-    	    FROM truong
-    	    JOIN hoc ON truong.matr = hoc.matr
-    	    JOIN hs ON hoc.mahs = hs.mahs
-    	    WHERE truong.tentr = '{ten_truong}' AND hoc.namhoc = '{nam_hoc}' AND hoc.xeploai = '{xep_loai}';
-        """
-
-    cursor.execute(query)
-    results = cursor.fetchall()
-
-    # Tính thời gian truy vấn
-    end_time = time.time()
-    query_time = end_time - start_time
+-- tao csdl TRUONGHOC1
+create database TRUONGHOC1;
 
 
-    table = []
-
-    for row in results:
-        full_name = f"{row[0]} {row[1]}"
-        table.append([full_name, row[2], row[3], row[4], row[5]])
-
-    headers = ["Họ tên", "NTNS", "Điểm TB", "Xếp loại", "Kết quả"]
-
-    print(tabulate(table, headers, tablefmt="fancy_grid"))
-    print(f"Thời gian truy vấn: {query_time} giây")
-    #truonghoc1,THPT Lê Quý Đôn,2019-2020,Giỏi
-    # Tạo và xuất file XML
-    xml_root = ET.Element("students")
-    for row in results:
-        full_name = f"{row[0]} {row[1]}"
-        student_element = ET.SubElement(xml_root, "student")
-        ET.SubElement(student_element, "ho_ten").text = full_name
-        ET.SubElement(student_element, "ntns").text = row[2]
-        ET.SubElement(student_element, "diem_tb").text = str(row[3])
-        ET.SubElement(student_element, "xep_loai").text = row[4]
-        ET.SubElement(student_element, "ket_qua").text = row[5]
-
-    xml_tree = ET.ElementTree(xml_root)
-    xml_filename = f"{database_name}-{ten_truong}-{nam_hoc}-{xep_loai}.xml"
-    xml_tree.write(xml_filename)
-
-    # Đóng kết nối cơ sở dữ liệu
-    conn.close()
+--  dung csdl TRUONGHOC1
+USE TRUONGHOC2;
 
 
-# Sử dụng hàm truy vấn cơ sở dữ liệu
-query_database()
+-- tao bang TRUONG
+
+CREATE TABLE TRUONG (
+    MATR CHAR(3) PRIMARY KEY,
+
+    TENTR VARCHAR(128) NOT NULL,
+    DCHITR VARCHAR(256) default NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+-- tao bang HS
+
+CREATE TABLE HS (
+    MAHS char(9) PRIMARY KEY,  
+    HO VARCHAR(64) NOT NULL,
+    TEN VARCHAR(128) NOT NULL,
+    CCCD VARCHAR(12),
+    NTNS varchar(12) NOT NULL,
+    DCHI_HS VARCHAR(256) NOT NULL,
+    CONSTRAINT UQ_HS_CCCD UNIQUE (CCCD)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+
+
+-- tao bang hoc
+CREATE TABLE HOC (
+    MATR char(3),
+    MAHS char(9),
+    NAMHOC char(9),
+    DIEMTB char(4) NOT NULL,
+    XEPLOAI varchar(32) NOT NULL,
+    KQUA varchar(32) NOT NULL,
+    PRIMARY KEY (MATR, MAHS, NAMHOC),
+    FOREIGN KEY (MATR) REFERENCES TRUONG (MATR),
+    FOREIGN KEY (MAHS) REFERENCES HS (MAHS)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+# tạo cluster index cho các khóa đã được yêu cầu
+CREATE INDEX TRUONG_TENTR ON TRUONG(TENTR);
+CREATE INDEX HOC_XEPLOAI ON HOC(XEPLOAI);
+CREATE INDEX HOC_NAMHOC ON HOC(NAMHOC);
