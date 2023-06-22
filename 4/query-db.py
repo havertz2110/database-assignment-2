51bcc93175b2a4c59a527065a534d2ff07e05317
4nh_H4v3rtz <121730782+AnhHavertz@users.noreply.github.com>
2023-06-22 23:26:42 +0700

    Update query-db.py

--- 4/query-db.py
+++ 4/query-db.py
@@ -1,22 +1,23 @@
+# Khai báo các thư viện cần thiết / Declare necessary libraries
 import mysql.connector
 import xml.etree.ElementTree as ET
 import time
 from tabulate import tabulate
 
 def query_database():
-    # Người dùng nhập thông tin
+    # Người dùng nhập thông tin / User input
     input_str = input("Nhập tên cơ sở dữ liệu, tên lĩnh vực, năm học, xếp hạng học tập (cách nhau bằng dấu phẩy): ")
 
-    # Tách chuỗi nhập vào thành các phần tử riêng biệt
+    # Tách chuỗi nhập vào thành các phần tử riêng biệt / Splitting the input string into separate elements
     inputs = input_str.split(",")
 
-    # Gán giá trị cho các biến tương ứng
+    # Gán giá trị cho các biến tương ứng / Assigning values to respective variables
     database_name = inputs[0].strip()
     ten_truong = inputs[1].strip()
     nam_hoc = inputs[2].strip()
     xep_loai = inputs[3].strip()
 
-    # Thiết lập kết nối với cơ sở dữ liệu MySQL
+    # Thiết lập kết nối với cơ sở dữ liệu MySQL / Establishing a connection with the MySQL database
     conn = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -24,13 +25,13 @@ def query_database():
         database="truonghoc1"
     )
 
-    # Tạo con trỏ để thực hiện các truy vấn
+    # Tạo con trỏ để thực hiện các truy vấn / Creating a cursor to perform queries
     cursor = conn.cursor()
 
-    # Đo thời gian thực hiện truy vấn
+    # Đo thời gian thực hiện truy vấn / Measuring query execution time
     start_time = time.time()
 
-    # Truy vấn dữ liệu từ cơ sở dữ liệu
+    # Truy vấn dữ liệu từ cơ sở dữ liệu /  Querying data from the database
     query = f"""
             SELECT hs.ho, hs.ten, hs.ntns, hoc.diemtb, hoc.xeploai, hoc.kqua
     	    FROM truong
@@ -38,27 +39,31 @@ def query_database():
     	    JOIN hs ON hoc.mahs = hs.mahs
     	    WHERE truong.tentr = '{ten_truong}' AND hoc.namhoc = '{nam_hoc}' AND hoc.xeploai = '{xep_loai}';
         """
-
     cursor.execute(query)
     results = cursor.fetchall()
 
-    # Tính thời gian truy vấn
+    # Tính thời gian truy vấn/ Calculating query time
     end_time = time.time()
     query_time = end_time - start_time
 
-
+    #  Tạo biểu đồ để hiển thị kết quả truy vấn / Creating a table representation of the query results
     table = []
 
+    #  Lặp qua từng dòng trong kết quả / Iterating over each row in the results
     for row in results:
+        # Kết hợp cột đầu tiên và cột thứ hai để tạo họ tên đầy đủ / Combining the first and second columns to form the full name
         full_name = f"{row[0]} {row[1]}"
+        # Thêm một dòng mới vào biểu đồ với các cột mong muốn / Appending a new row to the table with the desired columns
         table.append([full_name, row[2], row[3], row[4], row[5]])
 
+    # Định nghĩa tiêu đề cho biểu đồ / Defining the headers for the table
     headers = ["Họ tên", "NTNS", "Điểm TB", "Xếp loại", "Kết quả"]
-
+    
+    # In kết quả ra 1 bảng đã được định dạng / Printing the results in a formatted table
     print(tabulate(table, headers, tablefmt="fancy_grid"))
     print(f"Thời gian truy vấn: {query_time} giây")
-    #truonghoc1,THPT Lê Quý Đôn,2019-2020,Giỏi
-    # Tạo và xuất file XML
+   
+    # Tạo và xuất file XML / Creating and exporting an XML file
     xml_root = ET.Element("students")
     for row in results:
         full_name = f"{row[0]} {row[1]}"
@@ -73,9 +78,9 @@ def query_database():
     xml_filename = f"{database_name}-{ten_truong}-{nam_hoc}-{xep_loai}.xml"
     xml_tree.write(xml_filename)
 
-    # Đóng kết nối cơ sở dữ liệu
+    # Đóng kết nối cơ sở dữ liệu / Closing the database connection
     conn.close()
 
 
-# Sử dụng hàm truy vấn cơ sở dữ liệu
+# Sử dụng hàm truy vấn cơ sở dữ liệu / Using the query_database function
 query_database()
